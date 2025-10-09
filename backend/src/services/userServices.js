184945c71e0b515ee4c445ab18bde88680a2b899
ansonpoh <ansonpoh0909@gmail.com>
2025-10-09 19:37:26 +0800

    added backend code

--- backend/src/services/userServices.js
+++ backend/src/services/userServices.js
@@ -2,12 +2,12 @@ import pool from "../config/db.js";
 import bcrypt from "bcrypt";
 
 const salt_rounds = 10;
-export async function register(username, email, password, bio) {
+export async function register_user(user_id, username, email, password, bio) {
     try {
         const salt = await bcrypt.genSalt(salt_rounds);
         const hashed_password = await bcrypt.hash(password, salt);
-        const query = `insert into users (username, email, password_hash, bio) values ($1, $2, $3, $4)`;
-        const values = [username, email, hashed_password, bio];
+        const query = `insert into users (user_id, username, email, password_hash, bio) values ($1, $2, $3, $4, $5, $6)`;
+        const values = [user_id, username, email, hashed_password, bio, 0];
         const result = await pool.query(query, values);
         return result.rowCount > 0;
     } catch (err) {
@@ -16,7 +16,7 @@ export async function register(username, email, password, bio) {
     }
 }
 
-export async function check_if_email_in_use(email) {
+export async function check_if_user_email_in_use(email) {
     try {
         const query = `select * from users where email = $1`;
         const values = [email];
@@ -33,6 +33,29 @@ export async function get_user_by_id(id) {
         const query = `select * from users where user_id = $1`;
         const values = [id];
         const result = await pool.query(query, values);
+        return result.rows;
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
+
+export async function get_user_by_email(email) {
+    try {
+        const query = `select * from users where email = $1`;
+        const values = [email];
+        const result = await pool.query(query, values);
+        return result.rows;
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
+
+export async function get_all_users() {
+    try {
+        const query = `select * from users`;
+        const result = await pool.query(query);
         return result.rows;
     } catch (err) {
         console.error(err);
