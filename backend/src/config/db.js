724a4cee90034bd7ede11a637e3a97afbdfa77c2
sitinur <sitinur.2024@computing.smu.edu.sg>
2025-10-09 17:56:37 +0800

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -11,12 +11,12 @@ const pool = new Pool({
 }); 
 
 // tests connection
-// try {
-//     const query = `select * from users`;
-//     let res = await pool.query(query);
-//     console.log(res.rows)
-// } catch (err) {
-//     console.log(err); 
-// }
+try {
+    const query = `select * from users`;
+    let res = await pool.query(query);
+    console.log(res.rows)
+} catch (err) {
+    console.log(err); 
+}
 
 export default pool;
