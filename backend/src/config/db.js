e1998b753be8c4bac544525c78fb7362cabd60d5
Joshua <joshua.cher.2024@smu.edu.sg>
2025-10-09 18:38:11 +0800

    components for maps

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -9,14 +9,14 @@ const { Pool } = pkg;
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
 }); 
-
+console.log(process.env.DATABASE_URL)
 // tests connection
-// try {
-//     const query = `select * from users`;
-//     let res = await pool.query(query);
-//     console.log(res.rows)
-// } catch (err) {
-//     console.log(err); 
-// }
+try {
+    const query = `select * from users`;
+    let res = await pool.query(query); 
+    console.log(res.rows)
+} catch (err) {
+    console.log(err); 
+}
 
 export default pool;
