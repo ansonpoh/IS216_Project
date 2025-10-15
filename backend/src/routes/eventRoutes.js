4a69445b25be18443c719442fa989a7a4ae866cc
ansonpoh <ansonpoh0909@gmail.com>
2025-10-15 15:56:56 +0800

    added a few endpoints

--- backend/src/routes/eventRoutes.js
+++ backend/src/routes/eventRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { get_all_categories_handler, get_all_events_handler, get_event_by_id_handler, get_events_of_org_handler } from "../controllers/eventController.js";
+import { get_all_categories_handler, get_all_events_handler, get_event_by_id_handler, get_events_by_category_handler, get_events_of_org_handler } from "../controllers/eventController.js";
 
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.get("/get_all_categories", get_all_categories_handler);
 router.get("/get_all_events", get_all_events_handler);
 router.get("/get_event_by_id", get_event_by_id_handler);
 router.get("/get_events_of_org", get_events_of_org_handler);
+router.get("/get_events_by_category", get_events_by_category_handler);
 
 export default router;
