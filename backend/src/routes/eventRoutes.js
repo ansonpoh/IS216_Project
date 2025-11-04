e0ba8a6b0565ac7f1b48b4282330ac224d751a6b
Anson <ansonpoh0909@gmail.com>
2025-11-04 12:38:52 +0800

    added route for publishing

--- backend/src/routes/eventRoutes.js
+++ backend/src/routes/eventRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { get_all_categories_handler, get_all_events_handler, get_event_by_id_handler, get_events_by_category_handler, get_events_by_region_handler, get_events_of_org_handler, get_all_regions_handler, create_event_handler, get_events_by_time_handler, get_filtered_events_handler, get_selectable_options_handler, event_data_modify_handler, signup_event_handler, check_if_user_signed_up_handler, get_registered_events_for_user_handler, get_registered_users_for_event_handler, update_registration_status_handler, get_all_published_events_handler } from "../controllers/eventController.js";
+import { get_all_categories_handler, get_all_events_handler, get_event_by_id_handler, get_events_by_category_handler, get_events_by_region_handler, get_events_of_org_handler, get_all_regions_handler, create_event_handler, get_events_by_time_handler, get_filtered_events_handler, get_selectable_options_handler, event_data_modify_handler, signup_event_handler, check_if_user_signed_up_handler, get_registered_events_for_user_handler, get_registered_users_for_event_handler, update_registration_status_handler, get_all_published_events_handler, update_publish_status_handler } from "../controllers/eventController.js";
 import { requireAuth } from "../config/auth.js";
 
 const router = express.Router();
@@ -23,5 +23,5 @@ router.post("/create_event", create_event_handler);
 router.post("/event_data_modify", event_data_modify_handler);
 router.post("/signup_event", signup_event_handler);
 router.post("/update_registration_status", update_registration_status_handler);
-
+router.post("/update_publish_status", update_publish_status_handler);
 export default router;
